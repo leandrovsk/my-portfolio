cf5b18f3fa292921e3afa85cfe6f5b013ef3e505
leandrovsk <contas.center@gmail.com>
2023-07-23 21:45:16 -0300

    fix: user data

--- src/utils/userData.ts
+++ src/utils/userData.ts
@@ -1,9 +1,9 @@
 export const userData = {
-  nameUser: "Samuel Leão",
-  githubUser: "samuelleaokenzie",
-  linkedinUser: "nekelpatrick",
-  whatsappNumber: "+5541988819028",
-  emailUser: "samuel@kenzie.com.br",
-  instagramUser: "samuelleaoui",
+  nameUser: "Leandro Veloso de Siqueira",
+  githubUser: "leandrovsk",
+  linkedinUser: "leandro-veloso-de-siqueira",
+  whatsappNumber: "+5581999718257",
+  emailUser: "leoveloso.eng@gmail.com",
+  instagramUser: "",
   facebookUser: "",
 };
